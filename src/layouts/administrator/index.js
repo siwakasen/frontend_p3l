3a26552d132038b583c85a1ed2e079ca41c38791
ApocalCrk <ferdyfirmansyah3026@gmail.com>
2024-04-17 18:00:43 +0700

    reconfig

--- src/layouts/administrator/index.js
+++ src/layouts/administrator/index.js
@@ -1,3 +1,30 @@
 import { styled, Container, Box, useTheme } from '@mui/material';
+import React, { useState } from 'react';
 
+const MainWrapper = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    minHeight: '100vh',
+    width: '100%'
+}));
 
+const PageWrapper = styled(Box)(({ theme }) => ({
+    display: "flex",
+    flexGrow: 1,
+    paddingBottom: "60px",
+    flexDirection: "column",
+    zIndex: 1,
+    backgroundColor: "transparent"
+}));
+
+const AdminLayout = ({ children }) => {
+    const theme = useTheme();
+    const [open, setOpen] = useState(false);
+
+    return (
+        <MainWrapper>
+            <PageWrapper>
+                {children}
+            </PageWrapper>
+        </MainWrapper>
+    );
+}

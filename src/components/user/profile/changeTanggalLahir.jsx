c2d0aaf650c882987f0abb003946713cac23a69a
siwakasen <siwakasen@gmail.com>
2024-05-06 03:03:13 +0700

    rm unused function

--- src/components/user/profile/changeTanggalLahir.jsx
+++ src/components/user/profile/changeTanggalLahir.jsx
@@ -21,13 +21,7 @@ export const TanggalLahir = () => {
     };
 
     const [tanggal_lahir, setTanggalLahir] = React.useState("");
-    function formatDate(date) {
-        const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, '0');
-        const day = String(date.getDate()).padStart(2, '0');
-        return `${year}-${month}-${day}`;
-    }
-
+    
     return (
         <>
             <Typography component={"button"} color="primary" variant="subtitle1" fontWeight={"400"} onClick={handleClickOpen}>Ubah Tanggal Lahir</Typography>
